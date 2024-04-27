4f8f2e60e7939eb5af0ec2311a9b7e09388a4b3b
MaxFrancis1 <max.francis2000@gmail.com>
2024-04-27 16:35:00 +0100

    Setting up models

---
+++ BlogSpot.Web/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogSpot.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public ICollection<BlogPost> blogPosts { get; set; }
+    }
+}
